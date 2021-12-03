2e91ecbf5232fdfd72bd5d22b984a1d9e4aed066
srikanth collaty <srikanthcollaty92@gmail.com>
2021-12-03 08:53:14 +0100

    modif class simulation

--- src/main/java/fr/lernejo/guessgame/Simulation.java
+++ src/main/java/fr/lernejo/guessgame/Simulation.java
@@ -3,78 +3,61 @@ package fr.lernejo.guessgame;
 import fr.lernejo.logger.Logger;
 import fr.lernejo.logger.LoggerFactory;
 
-import javax.lang.model.type.NullType;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.TimeZone;
 
 public class Simulation {
-    private long maximum;
+
     private final Logger logger = LoggerFactory.getLogger("simulation");
-    private final Player player;  //TODO add variable type
-    private long numberToGuess; //TODO add variable type
-    private long Max_value;
+    private final Player player;
+    private long numberToGuess;
+    private long maximum;
 
     public Simulation(Player player) {
         this.player = player;
-        this.numberToGuess = 0;//TODO implement me
-        this.Max_value=0;
+        this.numberToGuess = 0;
+        this.maximum = 0;
     }
 
-    public void initialize(long numberToGuess,long Max_value) { //public void initialize
-
+    public void initialize(long numberToGuess, long maxTry) {
         this.numberToGuess = numberToGuess;
-        this.Max_value = Max_value;
+        this.maximum = maxTry;
     }
 
     /**
      * @return true if the player have guessed the right number
      */
     private boolean nextRound() {
-        //TODO implement me
-        long user = player.askNextGuess();
-        if (user == numberToGuess)
-            return true;
+        long userGuess = player.askNextGuess();
+
+        if (userGuess == numberToGuess) return true;
         else {
-            player.respond(user < numberToGuess);
+            player.respond(userGuess < numberToGuess);
         }
-        return false;  //TODO implement me
+        return false;
     }
 
     public void loopUntilPlayerSucceed() {
-        //TODO implement me
+        boolean continueGame;
+        int tryNumber = 0;
 
-        boolean detail_game = true;
-        boolean Game_s;
-        int mynumber_increment = 0;
-        int seconnumber;
-        int number2;
-        long tmp_start = System.currentTimeMillis(); // commencement du jeu start
-
-
+        long gameBegin = System.currentTimeMillis();
         do {
-            mynumber_increment++;
+            tryNumber++;
+            continueGame = nextRound();
+        } while (!continueGame && tryNumber < maximum);
+        long gameEnd = System.currentTimeMillis();
 
-            detail_game = nextRound();
-            logger.log("player" + player);
-            logger.log("detail " + detail_game);
-        }while (!detail_game && mynumber_increment < maximum) ;
+        long timeInGame = gameEnd - gameBegin;
 
-            long tmp_fin = System.currentTimeMillis();
-            System.out.println("Calcul du temps");
-            long time_result = tmp_fin - tmp_start;
+        if (continueGame) {
+            logger.log("well done!!!!!!!!!!!!!");
+            logger.log("The number was " + numberToGuess);
+            logger.log("It took you " + tryNumber + " try again.");
+        } else logger.log("not good " + numberToGuess);
 
-        if (detail_game) {
-          //  logger.log("player" + player);
-           // logger.log("detail " + detail_game);
-        } else logger.log("numero du guess: " + numberToGuess);
-
-
-
-        System.out.println("Date au format mm:ss.SSS");
-        SimpleDateFormat simpledateformat = new SimpleDateFormat("mm:ss.SSS");
-        Date date_result = new Date(time_result);
-        logger.log("time detail "+simpledateformat.format(date_result));
+        SimpleDateFormat sdf = new SimpleDateFormat("mm:ss.SSS");
+        Date resultTime = new Date(timeInGame);
+        logger.log("Time in-game: " + sdf.format(resultTime));
     }
 }
-
